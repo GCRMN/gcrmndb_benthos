d5b4a507d8d7732e34af70b908a2c725704bb10c
JWicquart <jeremywicquart@gmail.com>
2023-02-07 09:30:35 +0100

    standardize dataset 0009

--- code/01_standardization/0009.R
+++ code/01_standardization/0009.R
@@ -2,125 +2,72 @@
 
 library(tidyverse) # Core tidyverse packages
 library(readxl) # To read excel files
+library(lubridate) # For dates format
 
-dataset <- "0007" # Define the dataset_id
+dataset <- "0009" # Define the dataset_id
 
 # 2. Import, standardize and export the data ----
 
 # 2.1 Site data --
 
 data_site <- read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
-  filter(dataset_id == dataset & data_type == "site") %>% 
+  filter(datasetID == dataset & data_type == "site") %>% 
   select(data_path) %>% 
   pull() %>%
   # Read the file
   read_csv(file = .) %>% 
   select(surveyid, transectid, surveydate) %>% 
-  mutate(surveydate = as.Date(as.character(surveydate), format = "%Y%m%d")) %>% 
-  mutate(depth = 10) # Based on the value given in the data paper
+  rename(eventDate = surveydate) %>% 
+  mutate(eventDate = as.Date(as.character(eventDate), format = "%Y%m%d"),
+         year = year(eventDate),
+         month = month(eventDate),
+         day = day(eventDate),
+         verbatimDepth = 10) # Based on the value given in the data paper
 
 # 2.2 Code data --
 
 data_code <- read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
-  filter(dataset_id == dataset & data_type == "code") %>% 
+  filter(datasetID == dataset & data_type == "code") %>% 
   select(data_path) %>% 
   pull() %>%
   # Read the file
   read_csv(file = .) %>% 
-  select(region, label, func_group, label_name)
+  mutate(organismID = paste0(func_group, " - ", label_name)) %>% 
+  select(region, label, organismID) %>% 
+  mutate(region_id = as.character(as.numeric(as.factor(region))))
 
 # 2.3 Main data --
 
-data_site <- read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
-  filter(dataset_id == dataset & data_type == "main") %>% 
+# 2.3.1 Extract paths -
+
+data_main <- read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
+  filter(datasetID == dataset & data_type == "main") %>% 
   select(data_path) %>% 
-  pull() %>% 
-  map_dfr(., ~read_csv(file = .x)) %>% 
-  pivot_longer(6:ncol(.), values_to = "cover", names_to = "category")
+  mutate(region = str_split_fixed(data_path, "_", 4)[,4],
+         region = str_remove_all(region, ".csv"),
+         region = str_replace_all(region, c("atlantic" = "Atlantic",
+                                            "indianocean" = "Indian Ocean",
+                                            "pacificaustralia" = "Pacific Australia",
+                                            "pacifichawaii" = "Pacific Hawaii",
+                                            "southeastasia" = "Southeast Asia")),
+         region_id = as.character(1:5))
 
+# 2.3.2 Combine and transform data -
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 2.1 Site data --
-
-data_site <- read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
-  filter(dataset_id == dataset & data_type == "site") %>% 
-  select(data_path) %>% 
-  pull() %>% 
-  # Read the file
-  read.csv2(file = .) %>% 
-  rename(site = Site, lat = Latitude, long = Longitude, zone = Zone, depth = Depth)
-
-# 2.2 code data --
-
-data_code <- read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
-  filter(dataset_id == dataset & data_type == "code") %>% 
-  select(data_path) %>% 
-  pull() %>%
-  # Read the file
-  read.csv2(file = .)
-
-# 2.3 Main data --
-
-read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
-  filter(dataset_id == dataset & data_type == "main") %>% 
-  select(data_path) %>% 
-  pull() %>% 
-  # Read the file
-  read_xlsx(path = ., 
-            sheet = "Brut", 
-            col_types = c("numeric", "text", "date", "text", "text", 
-                          "numeric", "numeric", "text", "text")) %>% 
-  left_join(., data_code) %>% # Merge main data with substrates codes
-  select(-Season, -Substrate, -Remarques) %>% # Delete useless variables
-  rename(year = Year, date = Date, zone = Habitat, observer = Oserver, 
-         replicate = Station, taxid = Tax_ID) %>% # Rename variables
-  group_by(year, date, observer, zone, replicate, taxid) %>% 
-  count(name = "cover") %>% 
-  ungroup() %>% 
-  group_by(year, date, observer, zone, replicate) %>% 
-  mutate(total = sum(cover)) %>% 
-  ungroup() %>% 
-  mutate(dataset_id = dataset,
-         location = "Moorea", 
-         cover = (cover/total)*100,
-         method = "Point intersect transect, 50 m transect length, every 1 m",
-         taxid = str_to_sentence(str_squish(str_trim(taxid, side = "both")))) %>% 
-  select(-total) %>% 
+map_dfr(data_main$data_path, ~read_csv(file = .x), .id = "region_id") %>% 
+  pivot_longer(7:ncol(.), values_to = "measurementValue", names_to = "label") %>% 
+  drop_na(measurementValue) %>% 
+  filter(measurementValue != 0) %>% 
+  left_join(., data_code) %>% 
   left_join(., data_site) %>% 
+  select(-region_id, -region, -label, -surveyid, -imageid) %>% 
+  rename(decimalLongitude = lng, decimalLatitude = lat, 
+         parentEventID = transectid, eventID = quadratid) %>% 
+  mutate(datasetID = dataset,
+         measurementValue = measurementValue*100,
+         samplingProtocol = "Photo-quadrat") %>%
   write.csv(., file = paste0("data/02_standardized-data/", dataset, ".csv"), row.names = FALSE)
 
 # 3. Remove useless objects ----
 
-rm(data_site, data_code)
+rm(data_site, data_code, data_main)
