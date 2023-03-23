8cfd3ff375162c90c89c59f33f1e75f4b12e1d5e
JWicquart <jeremywicquart@gmail.com>
2023-03-23 22:06:20 +0100

    function to convert coords to decimal format

---
+++ code/00_functions/convert_coords.R
@@ -0,0 +1,21 @@
+convert_coords <- base::Vectorize(function(x){
+  
+  x <- str_squish(x)
+  
+  degree <- as.numeric(str_split_fixed(x, "°|'|\"", 4)[,1])
+  minute <- as.numeric(str_replace(str_split_fixed(x, "°|'|\"", 4)[,2], ",", "."))
+  second <- as.numeric(str_replace(str_split_fixed(x, "°|'|\"", 4)[,3], ",", "."))
+  
+  if(is.na(second)){
+    
+    result <- degree + (minute/60)
+    
+  }else{
+    
+    result <- degree + (minute/60) + (second/3600)
+    
+  }
+  
+  return(result)
+  
+})
