fb81981907ba6f9134d9be0cc2c276b6b2ed6c2e
JWicquart <jeremywicquart@gmail.com>
2023-05-23 11:44:06 +0200

    function to reformat mermaid data

---
+++ code/00_functions/mermaid_converter.R
@@ -0,0 +1,33 @@
+mermaid_converter <- function(data){
+  
+  split_benthic_categories <- function(data){
+    
+    result <- str_split_fixed(data$percent_cover_by_benthic_category_avg, ",", n = Inf)
+    
+    result <- tibble(raw = as.vector(result)) %>% 
+      mutate(organismID = str_squish(str_split_fixed(raw, ": ", 2)[,1]), 
+             measurementValue = str_split_fixed(raw, ": ", 2)[,2]) %>% 
+      mutate_all(., ~str_replace_all(.x, "\\{|\\'|\\}", "")) %>% 
+      mutate(measurementValue = as.numeric(measurementValue)) %>% 
+      select(-raw)
+    
+    return(result)
+    
+  }
+  
+  mermaid_reformat <- function(data){
+    
+    result <- bind_cols(data, split_benthic_categories(data))
+    
+    return(result)
+    
+  }
+  
+  mermaid_reformat(data) %>% 
+    select(project_id, site_name, latitude, longitude, sample_date_year, 
+           sample_date_month, sample_date_day, depth_avg, organismID, measurementValue) %>% 
+    rename(decimalLatitude = latitude, decimalLongitude = longitude, year = sample_date_year,
+           month = sample_date_month, day = sample_date_day, verbatimDepth = depth_avg, locality = site_name) %>% 
+    mutate(eventDate = as_date(paste(year, month, day, sep = "-")))
+  
+}
--- code/01_standardization/0010.R
+++ code/01_standardization/0010.R
@@ -3,17 +3,38 @@
 library(tidyverse) # Core tidyverse packages
 library(mermaidr) # API to mermaid data. To install -> remotes::install_github("data-mermaid/mermaidr")
 
-dataset <- "0008" # Define the dataset_id
+source("code/00_functions/mermaid_converter.R")
+
+dataset <- "0010" # Define the dataset_id
 
 # 2. Import, standardize and export the data ----
 
+A <- read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
+  filter(datasetID == dataset & data_type == "main") %>% 
+  select(data_path) %>% 
+  pull() %>% 
+  # Read the file
+  read_csv(.) %>% 
+  mutate(row = row_number()) %>% 
+  group_by(row) %>% 
+  group_modify(~mermaid_converter(data = .x)) %>% 
+  ungroup() %>% 
+  select(-row)
+
+
+############# TEST ###################
+
+
 projects <- mermaid_get_projects()
 
-
-
-data <- mermaid_get_project_data(project = "fe3f915c-ffd3-4290-a413-85672a8946e2")
-
+data <- mermaid_get_project_data(project = "60dd6ca0-d9d5-4f81-a3b7-7ac88ab3519c")
 
 wcs_mozambique <- projects %>%
-  filter(name == "WCS Mozambique Coral Reef Monitoring") %>%
+  filter(name == "2013-2014_Koro Island, Fiji") %>%
   mermaid_get_project_data(method = "fishbelt", data = "sampleevents")
+
+
+# 1. Access the data and download it in the appropriate folder
+# 2. Standardize the data using the function dedicated to MERMAID data
+# 3. Export the data to the folder standardized data
+
