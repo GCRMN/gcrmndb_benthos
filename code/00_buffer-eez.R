10fb281b2443517a180de51fc79278254b776f95
JWicquart <jeremywicquart@gmail.com>
2023-06-12 16:09:39 +0200

    improve country and territory assignment using EEZ spatial layer with buffer

---
+++ code/00_buffer-eez.R
@@ -0,0 +1,43 @@
+# 1. Load packages ----
+
+library(tidyverse)
+library(sf)
+sf_use_s2(FALSE)
+
+# 2. Load data ----
+
+data_eez <- st_read("data/07_data-eez/eez_v11.shp") %>% 
+  select(SOVEREIGN1, TERRITORY1) %>% 
+  #filter(TERRITORY1 %in% c("French Polynesia", "Hawaii", "Fiji", "Tuvalu", "Gilbert Islands")) %>% 
+  st_make_valid() %>% 
+  st_transform(crs = 4326) %>% 
+  st_make_valid() %>% 
+  st_transform(crs = 7801) %>% # CRS in meters
+  st_buffer(., dist = 5000) %>% # 5 km buffer
+  st_transform(crs = 4326) %>% 
+  st_wrap_dateline() %>% 
+  st_make_valid()
+
+ggplot() +
+  geom_sf(data = data_eez) +
+  coord_sf(xlim = c(170, 180))
+
+# 3. Correct issue for Hawaii EEZ ----
+
+data_eez_hawaii <- data_eez %>% 
+  filter(TERRITORY1 == "Hawaii") %>% 
+  st_wrap_dateline(options = c("WRAPDATELINE=YES", "DATELINEOFFSET=180"))
+  
+# 4. Add correction to other EEZ ----
+
+data_eez <- data_eez %>% 
+  filter(TERRITORY1 != "Hawaii") %>% 
+  bind_rows(., data_eez_hawaii)
+
+ggplot() +
+  geom_sf(data = data_eez) +
+  coord_sf(xlim = c(170, 180))
+
+# 5. Export the data ----
+
+st_write(data_eez, "data/07_data-eez/eez_v11_buffer_5km.shp", append = FALSE)
