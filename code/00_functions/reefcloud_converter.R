e8cf41e01a4a59b170b5d0c4e8cb10704f660f26
JWicquart <jeremywicquart@gmail.com>
2023-07-26 14:28:31 +0200

    function to convert reef cloud data format

---
+++ code/00_functions/reefcloud_converter.R
@@ -0,0 +1,20 @@
+reefcloud_converter <- function(data, datasetID = dataset){
+  
+  result <- data %>% 
+    pivot_longer(24:ncol(.), names_to = "organismID", values_to = "measurementValue") %>% 
+    filter(measurementValue != 0) %>% 
+    rename(eventDate = date, verbatimDepth = depth_m, decimalLongitude = site_longitude,
+           decimalLatitude = site_latitude, eventID = unique_id, parentEventID = transect,
+           habitat = site_reef_type, locality = site) %>% 
+    mutate(eventDate = as_date(dmy_hm(eventDate)),
+           year = year(eventDate),
+           month = month(eventDate),
+           day = day(eventDate),
+           datasetID = datasetID,
+           samplingProtocol = "Photo-quadrat") %>% 
+    select(datasetID, locality, parentEventID, eventID, decimalLatitude, decimalLongitude,
+           verbatimDepth, year, month, day, eventDate, samplingProtocol, organismID, measurementValue)
+  
+  return(result)
+  
+}
