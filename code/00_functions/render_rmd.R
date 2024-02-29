721f63f8b8b6df51a888f7ea9e2480f33757a178
JWicquart <jeremywicquart@gmail.com>
2024-02-29 14:39:31 +0100

    correct issue for categorisation of algae into subcategory

--- code/00_functions/render_rmd.R
+++ code/00_functions/render_rmd.R
@@ -2,6 +2,7 @@ render_rmd <- function(i){
   
   render("../code/00_functions/individual_checks.Rmd", 
          output_file = paste0("dataset_", i),
-         output_dir = "../output")
+         output_dir = "../output",
+         quiet = TRUE)
   
 }
