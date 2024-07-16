7afb3a30780770fc95bf0be8b08b4237cfd877e7
JWicquart <jeremywicquart@gmail.com>
2024-07-16 10:25:01 +0200

    export output for new datasets only

--- code/00_functions/render_rmd.R
+++ code/00_functions/render_rmd.R
@@ -1,8 +1,12 @@
 render_rmd <- function(i){
   
-  render("../code/00_functions/individual_checks.Rmd", 
-         output_file = paste0("dataset_", i),
-         output_dir = "../output",
-         quiet = TRUE)
+  if(file.exists(paste0("../output/dataset_", i, ".html")) == FALSE){
+    
+    render("../code/00_functions/individual_checks.Rmd", 
+           output_file = paste0("dataset_", i),
+           output_dir = "../output",
+           quiet = TRUE)
+    
+  }
   
 }
