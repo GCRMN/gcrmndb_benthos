dc75fd88fb128d9a12f0cafef29ecd4aa3841866
JWicquart <jeremywicquart@gmail.com>
2024-09-23 23:10:56 +0200

    update standardization for dataset 0080

--- code/01_standardization/0080.R
+++ code/01_standardization/0080.R
@@ -18,13 +18,46 @@ data_site <- read_csv("data/01_raw-data/benthic-cover_paths.csv") %>%
   select(locality, decimalLatitude, decimalLongitude) %>% 
   mutate(across(c(decimalLatitude, decimalLongitude), ~as.numeric(.)))
 
-## 2.2 Main data ----
+## 2.2 Depth data ----
 
-### 2.2.1 List of files to combine ----
+data_depth <- read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
+  filter(datasetID == dataset & data_type == "date") %>% 
+  select(data_path) %>% 
+  pull() %>% 
+  read_xlsx(sheet = 2) %>% 
+  rename(locality = Site, depth_min = "Depth (m) min", depth_max = "Depth (m) max") %>% 
+  mutate(verbatimDepth = (depth_min + depth_max)/ 2) %>% 
+  select(locality, verbatimDepth)
+
+## 2.3 Date data ----
+
+data_date <- read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
+  filter(datasetID == dataset & data_type == "date") %>% 
+  select(data_path) %>% 
+  pull() %>% 
+  read_xlsx() %>% 
+  rename(time = "Time Point", locality = Site, year = Year, month = Month, day = Day) %>% 
+  select(-Locality) %>% 
+  left_join(., data_depth) %>% 
+  mutate(eventDate = as_date(paste0(year,
+                                    str_pad(month, pad = "0", width = 2),
+                                    str_pad(day, pad = "0", width = 2),
+                                    sep = "-")),
+         time = zoo::na.locf(time),
+         locality = case_when(locality == "Coralina profunda" ~ "Coralina Profundo",
+                              locality == "Orbicella" ~ "Orbicellas",
+                              locality == "Palmata" ~ "Palmatas",
+                              locality == "Peñón" ~ "Penon",
+                              locality == "Fundemar" ~ "Vivero FUNDEMAR",
+                              TRUE ~ locality))
+
+## 2.3 Main data ----
+
+### 2.3.1 List of files to combine ----
 
 data_files <- list.files(path = "data/01_raw-data/0080/", pattern = ".xlsx", full.names = TRUE)
 
-### 2.2.2 Create a function to standardize data ----
+### 2.3.2 Create a function to standardize data ----
 
 convert_data_080 <- function(index_i, sheet_i = "cobertura"){
   
@@ -35,17 +68,18 @@ convert_data_080 <- function(index_i, sheet_i = "cobertura"){
     filter(!(organismID %in% c("Cobertura alga", "Cobertura coral vivo", "Suma"))) %>% 
     select(Site, Transect, organismID, measurementValue) %>% 
     rename(locality = Site, parentEventID = Transect) %>% 
-    mutate(year = as.numeric(str_split_fixed(data_files_i, n = 5, pattern = "_")[,4]))
+    mutate(time = str_split_fixed(data_files_i, n = 5, pattern = "_")[,3])
   
   return(data)
   
 }
 
-### 2.2.3 Map over the function ----
+### 2.3.3 Map over the function ----
 
 map_dfr(1:7, ~convert_data_080(index_i = .)) %>% 
   bind_rows(., convert_data_080(index_i = 8, sheet = 1)) %>% 
   mutate(datasetID = dataset,
+         samplingProtocol = "Photo-quadrat, 10 m transect length, every 2 m",
          locality = str_replace_all(locality, c("Cervivornis" = "Cervicornis",
                                                 "CocoReef" = "Coco Reef",
                                                 "CooReef" = "Coco Reef",
@@ -61,8 +95,10 @@ map_dfr(1:7, ~convert_data_080(index_i = .)) %>%
                               str_detect(locality, "Fundemar|FUNDEMAR") == TRUE ~ "Vivero FUNDEMAR",
                               TRUE ~ locality)) %>% 
   left_join(., data_site) %>% 
+  left_join(., data_date) %>%
+  select(-time) %>% 
   write.csv(., file = paste0("data/02_standardized-data/", dataset, ".csv"), row.names = FALSE)
 
 # 3. Remove useless objects ----
 
-rm(data_site, data_files, convert_data_080)
+rm(data_site, data_files, convert_data_080, data_depth, data_date)
