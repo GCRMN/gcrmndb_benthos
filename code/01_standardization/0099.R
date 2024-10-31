81b2bfd370a3768492ccc784c6cac64fff072078
JWicquart <jeremywicquart@gmail.com>
2024-10-31 11:15:58 +0100

    update standardization for dataset 0099

--- code/01_standardization/0099.R
+++ code/01_standardization/0099.R
@@ -11,13 +11,18 @@ read_csv("data/01_raw-data/benthic-cover_paths.csv") %>%
   select(data_path) %>% 
   pull() %>% 
   read.csv() %>% 
-  mutate(parentEventID = row_number()) %>% 
+  mutate(parentEventID = row_number(),
+         verbatimDepth = case_when(zone == "LT" ~ 10,
+                                   zone == "DO" ~ 5,
+                                   TRUE ~ NA)) %>% 
   rename(decimalLatitude = lat, decimalLongitude = lon, locality = wpt.no) %>% 
   select(-area, -zone) %>% 
   pivot_longer("cca":"turf", names_to = "organismID", values_to = "measurementValue") %>% 
   drop_na(measurementValue) %>% 
   mutate(measurementValue = measurementValue*100,
          datasetID = dataset,
-         samplingProtocol = "Photo-quadrat",
+         samplingProtocol = case_when(year %in% c(2014, 2017) ~ "Line intersect transect",
+                                      year %in% c(2020, 2023) ~ "Photo-quadrat",
+                                      TRUE ~ NA),
          locality = paste0("S", locality)) %>% 
   write.csv(., file = paste0("data/02_standardized-data/", dataset, ".csv"), row.names = FALSE)
