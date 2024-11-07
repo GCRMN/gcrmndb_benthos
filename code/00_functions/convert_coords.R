7bca41fd85bfc3ebf2efe81ec235578b01228300
JWicquart <jeremywicquart@gmail.com>
2024-11-07 16:11:21 +0100

    modify function to convert coordinates

--- code/00_functions/convert_coords.R
+++ code/00_functions/convert_coords.R
@@ -2,9 +2,9 @@ convert_coords <- base::Vectorize(function(x){
   
   x <- str_squish(x)
   
-  degree <- as.numeric(str_split_fixed(x, "°|'|\"", 4)[,1])
-  minute <- as.numeric(str_replace(str_split_fixed(x, "°|'|\"", 4)[,2], ",", "."))
-  second <- as.numeric(str_replace(str_split_fixed(x, "°|'|\"", 4)[,3], ",", "."))
+  degree <- as.numeric(str_split_fixed(x, "°|’|'|”|\"", 4)[,1])
+  minute <- as.numeric(str_replace(str_split_fixed(x, "°|’|'|”|\"", 4)[,2], ",", "."))
+  second <- as.numeric(str_replace(str_split_fixed(x, "°|’|'|”|\"", 4)[,3], ",", "."))
   
   if(is.na(second)){
     
