fd92964957b7c9b27ec963b8fe5ecd5c52355403
JWicquart <jeremywicquart@gmail.com>
2025-03-31 08:03:13 +0200

    standardize datasets 0188 to 0193

---
+++ code/01_standardization/0188.R
@@ -0,0 +1,21 @@
+# 1. Required packages ----
+
+library(tidyverse) # Core tidyverse packages
+library(readxl)
+
+dataset <- "0188" # Define the dataset_id
+
+# 2. Import, standardize and export the data ----
+
+read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
+  filter(datasetID == dataset & data_type == "main") %>%
+  select(data_path) %>% 
+  pull() %>% 
+  read_xlsx(., sheet = "data_with_summary") %>%
+  # Select the organization since multiple datasetID in a single Excel sheet
+  filter(Organization == "IMS") %>% 
+  rename(locality = Site, decimalLatitude = Latitude, decimalLongitude = Longitude, year = Year,
+         samplingProtocol = Method, organismID = `Benthic category`, measurementValue = mean_cover) %>% 
+  select(locality, decimalLatitude, decimalLongitude, year, samplingProtocol, organismID, measurementValue) %>% 
+  mutate(datasetID = dataset) %>% 
+  write.csv(., file = paste0("data/02_standardized-data/", dataset, ".csv"), row.names = FALSE)
