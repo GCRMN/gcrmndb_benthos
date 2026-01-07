18361f32b6abf9328d6085464de81ce0305a4426
JWicquart <jeremywicquart@gmail.com>
2026-01-07 16:48:54 +0100

    remove subregion name

--- code/01_clean-reefs.R
+++ code/01_clean-reefs.R
@@ -7,12 +7,13 @@ sf_use_s2(FALSE)
 # 2. Correct ETP coral reef sites (shared by Andrea Arriaga-Madrigual) ----
 
 read.csv("data/08_quality-checks-buffer/2025-10_regional_reef_type_data.csv") %>% 
-    filter(decimalLatitude >= -90 & decimalLatitude <= 90) %>%
-    filter(decimalLongitude < -74) %>% 
-    drop_na(decimalLatitude, decimalLongitude) %>% 
-    drop_na(reef_habitat_type) %>% 
-    filter(reef_habitat_type != "rocky_reef") %>% 
-    select(decimalLatitude, decimalLongitude, reef_habitat_type) %>% 
-    distinct() %>% 
-    st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = 4326) %>% 
-    st_write(., "data/08_quality-checks-buffer/gcrmndb-benthos_etp-reef-sites.shp", append = FALSE)
+  filter(decimalLatitude >= -90 & decimalLatitude <= 90) %>%
+  filter(decimalLongitude < -74) %>% 
+  drop_na(decimalLatitude, decimalLongitude) %>% 
+  drop_na(reef_habitat_type) %>% 
+  # "scattered_colonies", "other_formation", and "rocky_reef" removed :   
+  filter(reef_habitat_type %in% c("coral_community", "coral_reef")) %>% 
+  select(decimalLatitude, decimalLongitude, reef_habitat_type) %>% 
+  distinct() %>% 
+  st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = 4326) %>% 
+  st_write(., "data/08_quality-checks-buffer/gcrmndb-benthos_etp-reef-sites.shp", append = FALSE)
--- code/02_clean-buffer-reefs.js
+++ code/02_clean-buffer-reefs.js
@@ -27,9 +27,9 @@ var data_norfolk = ee.Geometry.Point([167.958552, -29.061416])
 
 // 4. Visual check ----
 
-Map.addLayer(data_norfolk, {color: '#e74c3c',  fillColor: '#e74c3c'});
-Map.addLayer(data_etp, {color: '#6c5ce7',  fillColor: '#a29bfe'});
-Map.addLayer(data_wri, {color: '#e1b12c',  fillColor: '#fbc531'});
+//Map.addLayer(data_norfolk, {color: '#e74c3c',  fillColor: '#e74c3c'});
+//Map.addLayer(data_etp, {color: '#6c5ce7',  fillColor: '#a29bfe'});
+//Map.addLayer(data_wri, {color: '#e1b12c',  fillColor: '#fbc531'});
 
 // 5. Merge data ----
 
@@ -39,7 +39,22 @@ var data_reefs = data_etp.merge(data_norfolk)
 
 //Map.addLayer(data_all, {color: '#6c5ce7',  fillColor: '#a29bfe'});
 
-// 6. Export shapefile ----
+// 6. Remove Galapagos, except Darwin and Wolf Islands ----
+
+var data_galapagos = ee.Geometry.Polygon([
+  [-92.61271, -2.13939],
+  [-88.54777, -2.13939],
+  [-88.54777,  0.82638],
+  [-92.61271,  0.82638],
+  [-92.61271, -2.13939]
+]);
+
+var data_reefs = data_reefs.map(function(f) {
+  var g = f.geometry().difference(data_galapagos, ee.ErrorMargin(1));
+  return ee.Feature(g).copyProperties(f);
+});
+
+// 7. Export shapefile ----
 
 Export.table.toDrive({
   collection: data_reefs,
@@ -49,20 +64,20 @@ Export.table.toDrive({
   fileFormat: "SHP"
 });
 
-// 7. Create 100 km buffer ----
+// 8. Create 100 km buffer ----
 
-// 7.1 Create a function to create the buffer ----
+// 8.1 Create a function to create the buffer ----
 
 var reef_buffer = function(feature) {
   return feature.buffer({distance: 100000, maxError: 1000}); // 100 km  
 };
 
-// 7.2 Map over the function ----
+// 8.2 Map over the function ----
 
 var reef_buffer = data_reefs.map(reef_buffer)
   .union(1);
 
-// 7.3 Export the data ----
+// 8.3 Export the data ----
 
 Export.table.toDrive({
   collection: reef_buffer,
