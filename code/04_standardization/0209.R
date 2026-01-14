e8a47b376f124ffb0e1c6b44b28870ea5117138c
JWicquart <jeremywicquart@gmail.com>
2026-01-14 08:22:13 +0100

    update standardisation for 0209

--- code/04_standardization/0209.R
+++ code/04_standardization/0209.R
@@ -7,11 +7,26 @@ dataset <- "0209" # Define the dataset_id
 
 # 2. Import, standardize and export the data ----
 
+## 2.1 Site data ----
+
+data_site <- read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
+  filter(datasetID == dataset & data_type == "site") %>%
+  select(data_path) %>% 
+  pull() %>% 
+  read_xlsx() %>% 
+  rename(decimalLatitude2 = decimalLatitude, decimalLongitude2 = decimalLongitude)
+
+## 2.2 Main data ----
+
 read_csv("data/01_raw-data/benthic-cover_paths.csv") %>% 
   filter(datasetID == dataset & data_type == "main") %>%
   select(data_path) %>% 
   pull() %>% 
   read_xlsx() %>% 
+  left_join(., data_site) %>%
+  mutate(decimalLatitude = coalesce(decimalLatitude, decimalLatitude2),
+         decimalLongitude = coalesce(decimalLongitude, decimalLongitude2)) %>%
+  select(-decimalLatitude2, -decimalLongitude2) %>% 
   select(-location, -scientificNameAccepted) %>% 
   rename(year = eventDate, organismID = functional_group) %>% 
   filter(organismID != "other") %>% 
@@ -28,3 +43,7 @@ read_csv("data/01_raw-data/benthic-cover_paths.csv") %>%
   mutate(samplingProtocol = "Photo-quadrat",
          datasetID = dataset) %>% 
   write.csv(., file = paste0("data/02_standardized-data/", dataset, ".csv"), row.names = FALSE)
+
+# 3. Remove useless objects ----
+
+rm(data_site)
